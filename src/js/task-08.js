46089f87ac7db9ea0894b9e6006954c863e64236
BorisKorsun <mainboko@gmail.com>
2022-02-06 22:46:06 +0200

    task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const formRef = document.querySelector('.login-form')
+const inputsRef = document.querySelectorAll('.login-form input')
+console.log(inputsRef)
+
+
+// inputsRef.addEventListener('input', event => {
+//     console.log(event.currentTarget.value === '')
+// })
+
+formRef.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault()
+    for (const input of inputsRef) {
+        if (input.value === '') {
+            alert('All the inputs have to be filled')
+        }
+    }
+    const formData = new FormData(event.currentTarget)
+    console.log(formData)
+    formRef.reset()
+}
 
