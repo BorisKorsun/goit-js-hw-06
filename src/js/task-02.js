1cf70f39ae779feb79029bc01ca8114273d37639
BorisKorsun <mainboko@gmail.com>
2022-02-06 14:47:51 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const createIngridients = array => {
+  return array.map(ingredient => {
+    const itemRef = document.createElement('li')
+    itemRef.textContent = ingredient
+    itemRef.classList.add('item')
+    return itemRef
+  })
+} 
+
+const elements = createIngridients(ingredients)
+console.log(elements)
+
+const listRef = document.querySelector('#ingredients')
+
+listRef.append(...elements)
