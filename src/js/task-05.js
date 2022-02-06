e6510277a296002aadb7f34baf6d5f0272e46280
BorisKorsun <mainboko@gmail.com>
2022-02-06 18:05:18 +0200

    task-05 fix

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,8 +1,12 @@
 const inputRef = document.querySelector('#name-input') 
 const nameRef = document.querySelector('#name-output')
-console.log(nameRef)
+
+console.log(inputRef.value)
 
 inputRef.addEventListener('input', onInput)
 function onInput(event) {
-    nameRef.textContent = event.currentTarget.value
+    if (event.currentTarget.value != '') {
+        return nameRef.textContent = event.currentTarget.value
+    }
+    return nameRef.textContent = 'Anonymous'
 }
