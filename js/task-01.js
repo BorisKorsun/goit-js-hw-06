16e6690dd4d8083e0f33831dcfd73fa7517fa98a
BorisKorsun <mainboko@gmail.com>
2022-02-09 12:30:14 +0200

    task-01-fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,9 @@
-const ulRef = document.querySelector('#categories')
-const liItemsRef = ulRef.children
+const ulRef = document.querySelector('#categories');
+const liItemsRef = document.querySelectorAll('.item');
 
-console.log(`Number of categories: ${liItemsRef.length}`)
+console.log(`Number of categories: ${liItemsRef.length}`);
 
-const makeCategory = obj => {
-    return `Category: ${obj.firstElementChild.textContent}`
-}
-const countElements = obj => {
-    const lastChild = obj.lastElementChild
-    return `Elements: ${lastChild.children.length}`
-}
-
-console.log(makeCategory(liItemsRef[0]))
-console.log(countElements(liItemsRef[0]))
-
-console.log(makeCategory(liItemsRef[1]))
-console.log(countElements(liItemsRef[1]))
-
-console.log(makeCategory(liItemsRef[2]))
-console.log(countElements(liItemsRef[2]))
-
-
+liItemsRef.forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}`)
+    console.log(`Elements: ${item.lastElementChild.children.length}`)
+})
