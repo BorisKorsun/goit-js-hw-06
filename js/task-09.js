03c8dbdc91cc39667c7e1737062d63b78e6e0307
BorisKorsun <mainboko@gmail.com>
2022-02-11 13:39:41 +0200

    task-09-fix

--- js/task-09.js
+++ js/task-09.js
@@ -6,10 +6,13 @@ const body = document.querySelector('body')
 console.log(body)
 const btnRef = document.querySelector('.change-color')
 console.log(btnRef)
+const spanTextRef = document.querySelector('.color')
+console.log(spanTextRef)
 
 btnRef.addEventListener('click', onBtnClick)
 
 function onBtnClick(event) {
   const randomColor = getRandomHexColor()
   body.style.backgroundColor = randomColor
+  spanTextRef.textContent = getRandomHexColor()
 }
